7a70c19fae452e75e903a86eee5df9233603d9a7
Louis Orleans <louis@orleans.io>
2025-09-11 14:44:15 -0700

    💥✨ add UUID schema

--- src/uuid.spec.ts
+++ src/uuid.spec.ts
@@ -1,16 +1,46 @@
 import { Effect, Exit, Schema as S } from 'effect';
 import { describe, it, expect } from '@effect/vitest';
 
-import { UUIDv4 } from './uuid.js';
+import { UUID, UUIDv4 } from './uuid.js';
 
 describe('uuid', () => {
+  describe('UUID', () => {
+    it.effect.each([
+      '00000000-0000-0000-0000-000000000000',
+      'B6BBAF20-1323-3711-8982-1D952875B310',
+      'B6BBAF20-1323-4711-8982-1D952875B310',
+      'D10746E4-52F9-4768-B2FA-254390F4F10A',
+      'C30CAE8E-8E05-4199-A900-1F222FCF550A',
+      '9C7D99C4-1387-4952-B042-AF1EFA3CAAB6',
+      'B6BBAF20-1323-5711-8982-1D952875B310',
+      'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF',
+    ] as (typeof UUID.Type)[])(`should parse valid UUID "%s"`, (str) =>
+      Effect.gen(function* () {
+        const result = yield* S.decode(UUID)(str).pipe(Effect.exit);
+        expect(result).toStrictEqual(Exit.succeed(str));
+      }),
+    );
+
+    it.effect.each([
+      '',
+      'no',
+      '000000000000000000000000000000000000',
+      '------------------------------------',
+    ])(`shouldn't parse invalid UUID "%s"`, (str) =>
+      Effect.gen(function* () {
+        const result = yield* S.decode(UUID)(str).pipe(Effect.exit);
+        expect(Exit.isFailure(result)).toBe(true);
+      }),
+    );
+  });
+
   describe('UUIDv4', () => {
     it.effect.each([
       'B6BBAF20-1323-4711-8982-1D952875B310',
       'D10746E4-52F9-4768-B2FA-254390F4F10A',
       'C30CAE8E-8E05-4199-A900-1F222FCF550A',
       '9C7D99C4-1387-4952-B042-AF1EFA3CAAB6',
-    ] as (typeof UUIDv4.Type)[])(`should parse valid email "%s"`, (str) =>
+    ] as (typeof UUIDv4.Type)[])(`should parse valid UUIDv4 "%s"`, (str) =>
       Effect.gen(function* () {
         const result = yield* S.decode(UUIDv4)(str).pipe(Effect.exit);
         expect(result).toStrictEqual(Exit.succeed(str));
@@ -24,7 +54,7 @@ describe('uuid', () => {
       '------------------------------------',
       '00000000-0000-0000-0000-000000000000',
       'B6BBAF20-1323-A711-8982-1D952875B310',
-    ] as (typeof UUIDv4.Type)[])(`shouldn't parse invalid email "%s"`, (str) =>
+    ])(`shouldn't parse invalid UUIDv4 "%s"`, (str) =>
       Effect.gen(function* () {
         const result = yield* S.decode(UUIDv4)(str).pipe(Effect.exit);
         expect(Exit.isFailure(result)).toBe(true);
--- src/uuid.ts
+++ src/uuid.ts
@@ -1,14 +1,28 @@
-import { ParseResult, Schema as S } from 'effect';
+import { ParseResult, Schema as S, Match } from 'effect';
 
-type UUIDv4 = `${string}-${string}-${string}-${string}-${string}`;
-function isUUIDv4(str: string): str is UUIDv4 {
+function isNilUUID<
+  Masquerade extends string = '00000000-0000-0000-0000-000000000000',
+>(str: string): str is Masquerade {
+  return str === '00000000-0000-0000-0000-000000000000';
+}
+
+function isOmniUUID<
+  Masquerade extends string = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF',
+>(str: string): str is Masquerade {
+  return str === 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF';
+}
+
+export type UUID = `${string}-${string}-${string}-${string}-${string}`;
+function isUUID(str: string): str is UUID {
   return (
+    isNilUUID(str) ||
+    isOmniUUID(str) ||
     str.match(
-      /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/iu,
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/iu,
     ) != null
   );
 }
-export const UUIDv4: S.Schema<UUIDv4, string> = S.transformOrFail(
+export const UUID: S.Schema<UUID, string> = S.transformOrFail(
   S.String,
   S.TemplateLiteral(
     S.String,
@@ -24,11 +38,62 @@ export const UUIDv4: S.Schema<UUIDv4, string> = S.transformOrFail(
   {
     strict: true,
     decode: (input, options, ast) =>
-      isUUIDv4(input)
+      isUUID(input)
         ? ParseResult.succeed(input)
         : ParseResult.fail(
-            new ParseResult.Type(ast, input, `Input is not a UUIDv4`),
+            new ParseResult.Type(ast, input, `Input is not a UUID`),
           ),
     encode: (s) => ParseResult.succeed(s),
   },
 );
+
+export type UUIDv4 = `${string}-${string}-4${string}-${string}-${string}`;
+function isUUIDv4(str: string): str is UUIDv4 {
+  return (
+    str.match(
+      /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/iu,
+    ) != null
+  );
+}
+export const UUIDv4: (opts: {
+  includeSpecial: boolean;
+}) => S.Schema<UUIDv4, string> = (
+  { includeSpecial } = { includeSpecial: true },
+) =>
+  S.transformOrFail(
+    S.String,
+    S.TemplateLiteral(
+      S.String,
+      '-',
+      S.String,
+      '-4',
+      S.String,
+      '-',
+      S.String,
+      '-',
+      S.String,
+    ),
+    {
+      strict: true,
+      decode: (input, options, ast) =>
+        Match.value({
+          v4: isUUIDv4(input),
+          includeSpecial,
+          nil: isNilUUID<UUIDv4>(input),
+          omni: isOmniUUID<UUIDv4>(input),
+        }).pipe(
+          Match.whenOr(
+            { v4: true },
+            { includeSpecial: true, nil: true },
+            { includeSpecial: true, omni: true },
+            () => ParseResult.succeed(input as UUIDv4),
+          ),
+          Match.orElse(() =>
+            ParseResult.fail(
+              new ParseResult.Type(ast, input, `Input is not a UUIDv4`),
+            ),
+          ),
+        ),
+      encode: (s) => ParseResult.succeed(s),
+    },
+  );
